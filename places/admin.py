b07a869a0948cface208a6a7ca335b64883d98c9
b10t <b10t.rus@gmail.com>
2022-06-04 17:58:53 +0800

    Add Place, PlaceImage models.

--- places/admin.py
+++ places/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Place, PlaceImage
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+    list_display_links = ('title', )
+    search_fields = ('title', )
+
+
+@admin.register(PlaceImage)
+class PlaceImageAdmin(admin.ModelAdmin):
+    list_display = ('place', )
+    list_display_links = ('place', )
+    search_fields = ('place', )
--- places/models.py
+++ places/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Place(models.Model):
+    title = models.CharField(
+        max_length=150,
+        verbose_name='Наименование'
+    )
+    description_short = models.CharField(
+        max_length=500,
+        verbose_name='Краткое описание'
+    )
+    description_long = models.TextField(
+        verbose_name='Описание'
+    )
+    latitude = models.FloatField(
+        default=0,
+        verbose_name='Широта'
+    )
+    longitude = models.FloatField(
+        default=0,
+        verbose_name='Долгота'
+    )
+
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Место'
+        verbose_name_plural = 'Места'
+
+
+class PlaceImage(models.Model):
+    place = models.ForeignKey(
+        Place,
+        on_delete=models.CASCADE,
+        verbose_name='Место'
+    )
+    image = models.ImageField(
+        verbose_name='Изображение'
+    )
+
+    def __str__(self) -> str:
+        return f'{self.place.title} #{self.pk}'
+
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
