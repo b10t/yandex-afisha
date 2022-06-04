3fe4f26b2bfc49d2625852035722db70c198e89c
b10t <b10t.rus@gmail.com>
2022-06-04 14:56:03 +0800

    Using environment variables.

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+from environs import Env
+
+env = Env()
+env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,14 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wm^o&xoip!1g$bgi@@ah9^=rg-!_*ddolfv35aurr+s29!bm(o'
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", [])
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env.str("SECRET_KEY", "REPLACE_ME")
 
-ALLOWED_HOSTS = []
-
+DEBUG = env.bool("DEBUG", True)
 
 # Application definition
 
