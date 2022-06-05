81ca56f158d1ff319be7e18fb4bf6c23c0d9c35a
b10t <b10t.rus@gmail.com>
2022-06-05 13:30:28 +0800

    Display preview image in admin page.

--- places/admin.py
+++ places/admin.py
@@ -1,10 +1,22 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 
 from .models import Place, PlaceImage
 
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
+    fields = ('image', 'preview_image', 'position', )
+
+    readonly_fields = ['preview_image']
+
+    def preview_image(self, obj):
+        return mark_safe(
+            '<img src="{url}" height={height} />'.format(
+                url=obj.image.url,
+                height=200,
+            )
+        )
 
 
 @admin.register(Place)
@@ -23,3 +35,13 @@ class PlaceImageAdmin(admin.ModelAdmin):
     list_display = ('position', 'place', )
     list_display_links = ('place', )
     search_fields = ('place', )
+
+    readonly_fields = ['preview_image']
+
+    def preview_image(self, obj):
+        return mark_safe(
+            '<img src="{url}" height={height} />'.format(
+                url=obj.image.url,
+                height=200,
+            )
+        )
