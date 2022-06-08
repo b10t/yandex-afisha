c32740504c3a7fe3baae2b39981032acc21f93b0
b10t <b10t.rus@gmail.com>
2022-06-09 06:58:36 +0800

    Fix urlpatterns.

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -21,7 +21,7 @@ from places.views import index, place
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('places/<int:place_id>', place, name='places'),
+    path('places/<int:place_id>/', place, name='places'),
     path('tinymce/', include('tinymce.urls')),
     path('', index),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
