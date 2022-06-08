708bd2be2b96ae1db3b5a6ebef9ce901be39b6ef
b10t <b10t.rus@gmail.com>
2022-06-09 06:46:29 +0800

    Removed the default value for SECRET_KEY.

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", [])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', [])
 
-SECRET_KEY = env.str("SECRET_KEY", "REPLACE_ME")
+SECRET_KEY = env.str('SECRET_KEY')
 
-DEBUG = env.bool("DEBUG", False)
+DEBUG = env.bool('DEBUG', False)
 
 # Application definition
 
