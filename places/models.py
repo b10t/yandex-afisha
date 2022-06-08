c42896476a077e6e5baf3e5823cc02e48a87434d
b10t <b10t.rus@gmail.com>
2022-06-09 07:24:03 +0800

    Allowed some fields to be empty.

--- places/models.py
+++ places/models.py
@@ -9,9 +9,11 @@ class Place(models.Model):
     )
     description_short = models.CharField(
         max_length=500,
+        blank=True,
         verbose_name='Краткое описание'
     )
     description_long = HTMLField(
+        blank=True,
         verbose_name='Описание'
     )
     latitude = models.FloatField(
